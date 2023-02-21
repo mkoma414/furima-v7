fe67ee4b58900ce1c9d24ebe286ade3bc77f35d7
Komatsubara Masato <m-koma@nifty.com>
2023-02-21 14:43:40 +0900

    JavaScriptのimport方法を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./calculate_price"
-import "./card"
+import "./custom/calculate_price"
+import "./custom/card"
 
